ed27c7cdba8f3c8b9c7c784c1be08bb3f2c8fab1
Marshular <marshy0909@gmail.com>
2023-02-05 14:53:25 -0800

    add

---
+++ increased_fire/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+data_file 'WEAPON_ANIMATIONS_FILE' 'data/**/weaponanimations.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'data/**/weaponcomponents.meta'
+data_file 'WEAPONINFO_FILE' 'data/**/weapons.meta'
